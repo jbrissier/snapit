25a2fb69f77b1aefabc68feac757b31bd4b76ebb
Jochen Brissier <j.brissier@gmail.com>
2014-08-18 22:37:07 +0200

    remove js stuff and make the upload working

--- snapit/views.py
+++ snapit/views.py
@@ -5,19 +5,21 @@ from django.template import RequestContext
 
 
 def handle_uploaded_file(f):
-    import pdb; pdb.set_trace()
-    with open('/Users/jochenbrissier/Desktop/test.jpg', 'wb+') as destination:
+    #import pdb; pdb.set_trace()
+    with open('/Users/jochen/Desktop/test.jpg', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
 
 def upload_file(request):
     if request.method == 'POST':
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
+            # new form
+            form = UploadFileForm()
+            return render_to_response('upload.html', {'form': form, 'thanks': 'Danke'}, RequestContext(request, {}))
         else:
             print 'form not valid'
     else:
