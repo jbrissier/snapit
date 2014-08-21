0b3fce99459e9b41947b8e38997c5e980de9e007
Jochen Brissier <j.brissier@me.com>
2014-08-21 22:36:47 +0200

    adding new urls

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from snapit.views import upload_file
+from snapit.views import upload_file, view_images
 
 admin.autodiscover()
 
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     # admin page
     #url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^$', upload_file, name="snapit_upload"),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^view/', view_images, name="snapid_view"),
+    url(r'^admin/', include(admin.site.urls)),
 )
