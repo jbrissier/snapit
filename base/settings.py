2dcd59f95612d7e8e306d407cd867aaa8e9afb4e
Jochen Brissier <j.brissier@me.com>
2015-05-15 15:10:18 +0200

    remove south

--- base/settings.py
+++ base/settings.py
@@ -66,7 +66,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/opt/static'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -155,7 +155,6 @@ EXTERNAL_APPS = [
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'tinymce',
     'base',
     'compressor',
